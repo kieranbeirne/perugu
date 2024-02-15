73e74cd3efbdf564535bfead088c4dc2e13224c5
kieranbeirne <kieranbeirne01@gmail.com>
2024-02-15 14:08:53 -0600

    added about us, contact body and footer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import Header from '../components/Header';
 import './HomePage.css'; // Ensure the HomePage.css file exists for styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
 const HomePage = () => {
@@ -17,12 +19,45 @@ const HomePage = () => {
           <h2 className="overlay-subtitle">The <u>Gut</u> it Takes to Run</h2>
         </div>
         <section id="about-us" className="section about-us">
+        <h2>About Us</h2>
         <div className="about-container">
-          <img src="/kieran-pfp.jpeg" alt="Your Name" className="about-image" />
+          <img src="/kieran-pfp.jpeg" alt="Kieran Beirne" className="about-image" />
           <h3 className="about-name">Kieran Beirne</h3>
           <p className="about-bio">
             Here is my bio. I've done this and that and am passionate about running.
-            {/* Your actual bio goes here */}
+          </p>
+        </div>
+        <br></br>
+        <hr width="40%"></hr>
+        <br></br>
+        <br></br>
+        <div className="about-container">
+          <img src="/satwik-pfp.jpg" alt="Satwik Putluri" className="about-image" />
+          <h3 className="about-name">Satwik Putluri</h3>
+          <p className="about-bio">
+            Howdy, my name is Satwik Putluri, and I am currently a sophomore studying biomedical sciences while pursuing a business minor. My career goal is to practice medicine while also conducting research. I am excited to work with my teammates to better understand the relation between the gut microbiome and running endurance as the field of gut micro bacteria is a growing field in the healthcare industry.
+          </p>
+        </div>
+        <br></br>
+        <hr width="40%"></hr>
+        <br></br>
+        <br></br>
+        <div className="about-container">
+          <img src="/satwik-pfp.jpg" alt="Janak Abraham" className="about-image" />
+          <h3 className="about-name">Janak Abraham</h3>
+          <p className="about-bio">
+            Howdy, my name is Janak Abraham, and I am currently a sophomore Biomedical Science major with a minor in business. I aspire to become a sports medicine physician and work alongside athletes to better their performance. Utilizing the theory “food is medicine” my teammates and I hope to become a driving force in completely understanding the capabilities of the gut microbiome.
+          </p>
+        </div>
+        <br></br>
+        <hr width="40%"></hr>
+        <br></br>
+        <br></br>
+        <div className="about-container">
+          <img src="/satwik-pfp.jpg" alt="Vikash Ravuri" className="about-image" />
+          <h3 className="about-name">Vikash Ravuri</h3>
+          <p className="about-bio">
+          Howdy, my name is Vikash Ravuri, and I am currently a sophomore studying biology with a business minor in pursuit of studying medicine. I am excited to work with my teammates to better understand the relation between nutrition and runners. I personally am a marathon runner and have come to understand the importance of having a well-balanced runner personalized diet. Runners are a specific niche that metabolize and require a different nutrient portfolio compared to the average person and we hope to make personalized nutrition at the forefront of runners daily life.
           </p>
         </div>
       </section>
@@ -40,8 +75,13 @@ const HomePage = () => {
       </section>
       <section id="contact-us" className="section contact-us">
         <h2>Contact Us</h2>
-        {/* Content for Contact Us */}
+        <div className="contact-details">
+          <FontAwesomeIcon icon={faEnvelope} /> <a href="mailto:perugu@gmail.com">perugu@gmail.com</a>
+        </div>
       </section>
+      <footer className="footer">
+        © 2024 Perugu. All rights reserved.
+      </footer>
     </div>
   );
 };
